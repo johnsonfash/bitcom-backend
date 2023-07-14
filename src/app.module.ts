2dc29d812d4a039a99e81d17ac3cd22780f1a863
Johnson Fash <fashanutosin7@gmail.com>
2023-07-14 17:40:38 +0100

    added test hello world module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { MysqlModule } from './mysql/mysql.module';
 import { LgaModule } from './lga/lga.module';
 import { PollingUnitModule } from './polling-unit/polling-unit.module';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), MysqlModule, LgaModule, PollingUnitModule]
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MysqlModule, LgaModule, PollingUnitModule, HelloModule]
 })
 export class AppModule { }
