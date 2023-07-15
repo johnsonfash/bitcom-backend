5e1dc65eef7e9d82dfff182615876933d36faa45
Johnson Fash <fashanutosin7@gmail.com>
2023-07-15 02:39:47 +0100

    added create #3

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { HttpExceptionFilter, TransformInterceptor } from './common';
 
 const PORT = process.env.PORT || 5000
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor())
