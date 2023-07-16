ab6e34f5f56d45fa505c4c9897f8a11735d256ec
Johnson Fash <fashanutosin7@gmail.com>
2023-07-16 20:57:13 +0100

    added deployment script

--- dist/common/constants.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const CONSTANTS: {
-    DATABASE_HOST: string;
-    DATABASE_USER: string;
-    DATABASE_PASSWORD: string;
-    DATABASE_NAME: string;
-};
--- dist/lga/lga.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { LgaService } from './lga.service';
-export declare class LgaController {
-    private lgaService;
-    constructor(lgaService: LgaService);
-    getAllLgas(): Promise<unknown>;
-    getLgaResult(id: number): Promise<unknown>;
-}
